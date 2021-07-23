586875dc56394ef0e14d4147052a2a235e8a382a
sharmaarchana <sharmaarchana@hotmail.com>
2021-07-23 14:07:07 +0200

    updated calculator.js. removed error addition function.

--- tests/calculator.js
+++ tests/calculator.js
@@ -24,26 +24,4 @@ describe('demo calculator test', function(){
 
     });
 
-    it('addition test', function(){
-
-        //browser.get('http://juliemr.github.io/protractor-demo/');
-        homepage.get('http://juliemr.github.io/protractor-demo/');
-
-        //element(by.model('first')).sendKeys('2');
-        homepage.enterFirstNumber('4');
-
-        //element(by.model('second')).sendKeys('3');
-        homepage.enterSecondNumber('3');
-
-        //element(by.css('[ng-click="doAddition()"]')).click();
-        homepage.clickGo1();
-
-        //let result = element(by.cssContainingText('.ng-binding', '5'));
-        //expect(result.getText()).toEqual('5');
-        homepage.verifyResult('7');
-
-        browser.sleep(3000);
-
-    });
-
 });
